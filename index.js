73863afcc30aa1945341b8f95273640b1e56fd37
ZakAttakk <36577338+ZakAttakk@users.noreply.github.com>
2022-10-12 18:46:37 -0400

    fixes

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const sayError = (error) => {
 
 getAlbums().then(result => console.log(result))
 
-// 1. In the code commented code below, write a unique word where it says typeWordHere.  Then, uncomment out the code below so that it AJAX does a POST request to the API.  The API should make a new "resource" (kind of like an API folder) on the backend that is the same name as your word.
+// 1. In the commented code below, write a unique word where it says "typeWordHere".  Then, uncomment out the code below so that $.ajax does a POST request to the API.  The API should make a new "resource" (kind of like an API folder) on the backend that is the same name as your word.
 
 // $.ajax({
 //   url: apiURL + "/typeWordHere",
@@ -35,7 +35,7 @@ getAlbums().then(result => console.log(result))
 
 // 2. Now comment back out the code above.  On line 5, change the word "albums" to your new word so that you can do a GET request and get all the data in your resource.
 
-// 3. In order to remove data from a resource, the API documentaion (on crudcrud.com) say that you need to use a DELETE request and that you need to specify an ID for whatever you are deleting.  Look at the GET request for yourm resource and try to find an ID for some data that you can delete.  Then change the words "albums" and "PUT-ID-HERE" below to  delete the data. 
+// 3. In order to remove data from a resource, the API documentaion (on crudcrud.com) says that you need to use a DELETE request and that you need to specify an ID in the API URL for whatever you are deleting.  Look at the GET request that you did for your resource and try to find an ID that you can use to delete data.  Then change the words "albums" and "PUT-ID-HERE" below to delete the data. 
 
 // $.ajax({
 //   url: apiURL + "/albums" + "/PUT-ID-HERE",
@@ -45,11 +45,11 @@ getAlbums().then(result => console.log(result))
 //   success: doStuffWithData
 // });
 
-// 4. Wrap the two jquery ajax function above in async functions so that you don't have to keep commenting and uncommenting them.
+// 4. Wrap the two jquery ajax functions above in async functions so that you don't have to keep commenting and uncommenting them everytime you want to add and delete.
 
 // 5. On the HTML page, make 3 buttons: Get, Add, and Delete.  Make the Get button run the GET request function.
 
-// 6. Finally, make two text inputs, one that will take an ID number (for deletion purposes) and one that will just take regular text (for addition purposes).  Make it so that the Add and Delete buttons can use the values in the input boxes to Add and Delete.  To do this you'll need to use many of the HTML and JavaScript skills you learned in passed lessons.
+// 6. Finally, on the HTML page make two text inputs, one that will take an ID number (for deletion purposes) and one that will just take regular text (for addition purposes).  Make it so that the Add and Delete buttons use the values in the input boxes to Add and Delete.  To do this you'll need to use many of the HTML and JavaScript skills you learned in past lessons.
 
 
 
